264503dd83972a920c2b281635037c711d69c329
Sideeyecat <sohamasme3@gmail.com>
2025-01-23 12:31:55 +0530

    Update and rename lineage_j2y18lte.mk to aosp_j2y18lte.mk

--- aosp_j2y18lte.mk
+++ aosp_j2y18lte.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from j4primelte device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +28,7 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := j2y18lte
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_NAME := lineage_j2y18lte
+PRODUCT_NAME := aosp_j2y18lte
 PRODUCT_MODEL := Galaxy J2 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
